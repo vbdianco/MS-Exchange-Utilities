757ff38d94ee821401cbf2a1ae70e0ede7a6ea04
Vincent Paul Dianco <vbdianco@gmail.com>
2026-02-10 15:35:22 +0800

    Updated EXOConn.ps1 to check if Exchange Online Management is installed before executing.

--- Conn/EXOConn.ps1
+++ Conn/EXOConn.ps1
@@ -1,3 +1,19 @@
-$usrCred = Get-Credential
-Connect-ExchangeOnline -UserPrincipalName $usrCred.UserName -ShowProgress $true
-$Host.UI.RawUI.WindowTitle = "Exchange Online"
+if(-not(Get-Module ExchangeOnlineManagement -ListAvailable)){
+    Write-Host "Exchange Online PowerShell module is not installed."
+    Write-Host "Installing required module..."
+
+    try{
+        Import-Module -Name ExchangeOnlineManagement -ErrorAction Stop
+        Write-Host "Exchange Online PowerShell module installed successfully."
+        $usrCred = Get-Credential
+        Connect-ExchangeOnline -UserPrincipalName $usrCred.UserName -ShowProgress $true
+        $Host.UI.RawUI.WindowTitle = "Exchange Online"        
+    }
+    catch {
+        Write-Host "Error installing module: $($_.Exception.Message)"
+    }
+}else{
+    $usrCred = Get-Credential
+    Connect-ExchangeOnline -UserPrincipalName $usrCred.UserName -ShowProgress $true
+    $Host.UI.RawUI.WindowTitle = "Exchange Online"
+}
